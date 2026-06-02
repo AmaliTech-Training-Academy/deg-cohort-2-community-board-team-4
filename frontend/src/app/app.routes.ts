b485572d4f283534cefc4a5e678807aa1b24ace0
axoo01 <juniorax03@gmail.com>
2026-06-02 09:32:50 +0200

    sec(auth): implement functional AuthGuards and JWT HTTP Interceptor #15

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard, publicGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [publicGuard],
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
   {
     path: 'dashboard',
+    canActivate: [authGuard],
     loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
-    
   },
   {
     path: '',
